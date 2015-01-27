76cf8a4bc28faeeb3ef388bad3f2fe2f340258a4
Caleb Thompson <caleb@calebthompson.io>
2015-01-26 19:17:58 -0600

    feedback

--- templates/puma.rb
+++ templates/puma.rb
@@ -3,15 +3,15 @@
 # The environment variable WEB_CONCURRENCY may be set to a default value based
 # on dyno size. To manually configure this value use heroku config:set
 # WEB_CONCURRENCY.
-workers Integer(ENV["WEB_CONCURRENCY"] || 3)
-threads_count = Integer(ENV["MAX_THREADS"] || 5)
+workers Integer(ENV.fetch("WEB_CONCURRENCY", 3))
+threads_count = Integer(ENV.fetch("MAX_THREADS", 5))
 threads(threads_count, threads_count)
 
 preload_app!
 
 rackup DefaultRackup
-port ENV["PORT"] || 3000
-environment ENV["RACK_ENV"] || "development"
+port ENV.fetch("PORT", 3000)
+environment ENV.fetch("RACK_ENV", "development")
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
